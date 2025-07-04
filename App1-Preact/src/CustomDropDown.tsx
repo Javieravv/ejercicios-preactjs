d50d9018d552f04053db90acdf31d3c5b83412f9
Javier Armando Vargas Vega - Mac <javieravargasestudiante@gmail.com>
2025-07-04 13:24:31 -0500

    Iniciando a construir componente dropdown menu personalizado

--- App1-Preact/src/CustomDropDown.tsx
+++ App1-Preact/src/CustomDropDown.tsx
@@ -1,10 +1,62 @@
-import { Dropdownmenu } from "@/components/DropDownMenu"
+import { Dropdownmenu, type OptionMenu } from "@/components/DropDownMenu"
+
+const menuPaises: OptionMenu[] = [
+    {
+        id: 'pais1',
+        label: 'Colombia',
+        url: ''
+    },
+    {
+        id: 'pais2',
+        label: 'Venezuela',
+        url: ''
+    },
+    {
+        id: 'pais3',
+        label: 'Ecuador',
+        url: ''
+    },
+    {
+        id: 'pais4',
+        label: 'Peru',
+        url: ''
+    },
+]
+
+const menuFrutas: OptionMenu[] = [
+    {
+        id: 'fruta1',
+        label: 'Manzana',
+        url: ''
+    },
+    {
+        id: 'fruta2',
+        label: 'Pera',
+        url: ''
+    },
+    {
+        id: 'fruta3',
+        label: 'Pitaya',
+        url: ''
+    },
+    {
+        id: 'fruta4',
+        label: 'Mandarina',
+        url: ''
+    },
+]
+
 
 export const CustomDropDown = () => {
     return (
         <>
-            <h1>Custom Drop.. Down Menu.....</h1>
-            <Dropdownmenu />
+            <h1>Custom Drop Down Menu.</h1>
+            <nav class='navbarprueba'>
+                <Dropdownmenu labelMenu="Paises" optionsMenu={menuPaises} />
+                <Dropdownmenu labelMenu="Frutas" optionsMenu={menuFrutas} />
+                <Dropdownmenu labelMenu="Verduras" />
+                <Dropdownmenu labelMenu="Opciones" />
+            </nav>
         </>
     )
 }
--- App1-Preact/src/components/DropDownMenu.tsx
+++ App1-Preact/src/components/DropDownMenu.tsx
@@ -1,11 +1,91 @@
+import '@/components/css/dropdown-menu.css'
+import { useEffect, useRef, useState } from 'preact/hooks'
 
-export const Dropdownmenu = () => {
-  return (
-    <div>
-      Drop Down Menu Component1111!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, eum.!
-    </div>
-  )
+export interface OptionMenu {
+    id: string;
+    label: string;
+    url: string;
+}
+
+interface DropDownProps {
+    labelMenu: string;
+    optionsMenu?: OptionMenu[]
+
+}
+
+// Creamos aqui el icono de la flecha
+interface DownArrowProps {
+    classArrow?: string;
+    sizeArrow?: number;
+}
+
+const DownArrow = ({
+    classArrow = '',
+    sizeArrow = 24
+}: DownArrowProps) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width={`${sizeArrow}px`}
+            height={`${sizeArrow}px`}
+            viewBox={`0 0 ${sizeArrow} ${sizeArrow}`}
+            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class={`${classArrow}`}>
+            <path d="m6 9 6 6 6-6" />
+        </svg>
+    )
+}
+
+
+// Aqui esta el contenido del dropdown men
+export const Dropdownmenu = ({
+    labelMenu,
+    optionsMenu = []
+}: DropDownProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const menuRef = useRef<HTMLDivElement>(null)
+
+    // Efecto para controlar cuando se de click fuera del menu
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+                setIsOpen(false)
+            }
+        }
+        document.addEventListener('click', handleClickOutside)
+        return () => document.removeEventListener('click', handleClickOutside)
+    }, [])
+
+    return (
+        <div class="dropdown-container" ref={menuRef}>
+            <div class="dropdown-trigger">
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {labelMenu}
+                    {optionsMenu.length > 0 &&
+                        <span>
+                            <DownArrow
+                                sizeArrow={24}
+                                classArrow={`${isOpen && 'arrowdown-rotate'} arrowdown`} />
+                        </span>
+                    }
+                </button>
+            </div>
+            {
+                optionsMenu.length > 0 && (
+                    <ul
+                        class={`dropdown-optionslist ${isOpen && 'activate'}`}
+                    >
+                        {
+                            optionsMenu.map((option) => {
+                                return <li class="dropdown-option" key={option.id}>{option.label}</li>
+                            })
+                        }
+                    </ul>
+                )
+            }
+        </div>
+    )
 }
 
 // export default Dropdownmenu
